f9ebef8e5d6fbd79dfc7dc0220d6d79a7a824fdd
haturatu <taro@eyes4you.org>
2025-09-28 19:18:03 +0900

    fix: 正常に書き込まれるように

--- speedtest-starlink.sh
+++ speedtest-starlink.sh
@@ -12,13 +12,14 @@ TMP_FILE="/tmp/speedtest.tmp"
   echo "###########################################"
   echo "-------------------------------------------"
   date
-  /usr/bin/speedtest-cli --secure --simple
 } > "$TMP_FILE"
+
+/usr/bin/speedtest-cli --secure --simple >> "$TMP_FILE"
 
 if [ -f "$OUTPUT_FILE" ]; then
     awk 'NR>6' "$OUTPUT_FILE" >> "$TMP_FILE"
 fi
 
 mv "$TMP_FILE" "$OUTPUT_FILE"
+rm -rf "$TMP_FILE"
 
-trap 'rm -f "$TMP_FILE"' EXIT
