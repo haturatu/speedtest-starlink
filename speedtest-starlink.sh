1f91ecc0f2c235263c25902dbf54f971c4e13f85
haturatu <taro@eyes4you.org>
2025-10-05 00:07:39 +0900

    fix: リトライ追加

--- speedtest-starlink.sh
+++ speedtest-starlink.sh
@@ -2,6 +2,8 @@
 
 OUTPUT_FILE="/var/www/starlink/starlink-jp"
 TMP_FILE="/tmp/speedtest.tmp"
+MAX_RETRIES=3
+SLEEP_SECONDS=60
 
 {
   echo "###########################################"
@@ -14,7 +16,21 @@ TMP_FILE="/tmp/speedtest.tmp"
   date
 } > "$TMP_FILE"
 
-/usr/bin/speedtest-cli --secure --simple >> "$TMP_FILE"
+attempt=1
+while [ $attempt -le $MAX_RETRIES ]; do
+    echo "speedtest-cli 実行: 試行 $attempt"
+    
+    OUTPUT=$( /usr/bin/speedtest-cli --secure --simple )
+    
+    if [ -n "$OUTPUT" ]; then
+        echo "$OUTPUT" >> "$TMP_FILE"
+        break
+    else
+        echo "結果が空です。$SLEEP_SECONDS 秒後にリトライします..."
+        sleep $SLEEP_SECONDS
+        attempt=$((attempt + 1))
+    fi
+done
 
 if [ -f "$OUTPUT_FILE" ]; then
     awk 'NR>6' "$OUTPUT_FILE" >> "$TMP_FILE"
@@ -22,4 +38,3 @@ fi
 
 mv "$TMP_FILE" "$OUTPUT_FILE"
 rm -rf "$TMP_FILE"
-
